38f135bfd1248e7fbcdd4bf10141bd3533a1dead
Christy Guan <cguan00@stuy.edu>
2019-10-27 15:00:24 -0400

    finished functions, should work now

--- linkedList.c
+++ linkedList.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "linkedList.h"
 
 //Should take a pointer to a node struct and print out all of the data in the list
@@ -25,10 +23,38 @@ struct node * insert_front(struct node * n, int data){
 
 
 //Should take a pointer to a list as a parameter and then go through the entire list freeing each node and return a pointer to the beginning of the list (which should be NULL by then).
-// struct node * free_list(struct node *);
+struct node * free_list(struct node *n){
+  struct node *temp;
+  while (n->next != 0){
+    printf("freeing node: %d\n", n->i);
+    temp = n->next;
+    free(n);
+    n = temp; 
+  }
+  return temp;
+}
 
 
 // Remove the node containing data from the list pointed to by front.
 // If data is not in the list, nothing is changed.
 // Returns a pointer to the beginning of the list.
-// struct node * remove(struct node *front, int data);
+struct node * remove_node(struct node *front, int data){
+    struct node *temp = front->next;
+    struct node *prev = front;
+    if (front ->i == data) {
+      front = NULL;
+      free(front);
+      return temp;
+    };
+    while (temp != NULL) {
+      if(temp -> i == data){
+        prev -> next = temp -> next;
+        free(temp);
+        temp = NULL;
+        return front;
+      }
+      prev = temp;
+      temp = temp -> next;
+    }
+    return front;
+}
