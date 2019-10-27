26c9b9321685130eaa54b79e68f0f90ad155c5d9
Christy Guan <cguan00@stuy.edu>
2019-10-27 15:00:48 -0400

    updated

--- linkedList.h
+++ linkedList.h
@@ -5,4 +5,4 @@ struct node { int i; struct node *next; };
 void print_list(struct node *);
 struct node * insert_front(struct node *, int);
 struct node * free_list(struct node *);
-struct node * remove(struct node *front, int data);
+struct node * remove_node(struct node *front, int data);
