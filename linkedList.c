c8fab30bc09951cc3fb3ab2277f3cdd60c5c9548
Christy Guan <cguan00@stuy.edu>
2019-10-27 11:12:59 -0400

    wrote insert_front

--- linkedList.c
+++ linkedList.c
@@ -1,6 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "linkedList.h"
+
 //Should take a pointer to a node struct and print out all of the data in the list
 void print_list(struct node * n){
-  printf("[ "); 
+  printf("[ ");
   while(n->next != 0){
     printf("%d ", n->i);
     n = n->next;
@@ -12,14 +16,19 @@ void print_list(struct node * n){
 // Should take a pointer to the existing list and the data to be added, create a new node and put it at the beginning of the list.
 // The second argument should match whatever data you contain in your nodes.
 // Returns a pointer to the beginning of the list.
-struct node * insert_front(struct node *, int);
+struct node * insert_front(struct node * n, int data){
+  struct node *first = malloc(sizeof(struct node));
+  first->i = data;
+  first->next = n;
+  return first;
+}
 
 
 //Should take a pointer to a list as a parameter and then go through the entire list freeing each node and return a pointer to the beginning of the list (which should be NULL by then).
-struct node * free_list(struct node *);
+// struct node * free_list(struct node *);
 
 
 // Remove the node containing data from the list pointed to by front.
 // If data is not in the list, nothing is changed.
 // Returns a pointer to the beginning of the list.
-struct node * remove(struct node *front, int data);
+// struct node * remove(struct node *front, int data);
